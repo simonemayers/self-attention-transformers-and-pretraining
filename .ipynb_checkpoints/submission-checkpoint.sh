2c38f63b4000e8d6694dd9b2a4bbfd83b17d7a42
simone <simone21295@gmail.com>
2025-04-09 09:34:52 -0400

    zipped

--- .ipynb_checkpoints/submission-checkpoint.sh
+++ .ipynb_checkpoints/submission-checkpoint.sh
@@ -1,3 +1,15 @@
 rm -f homework5.zip 
-rm -rf src/__pycache__
-zip -r homework5.zip src/ vanilla.model.params vanilla.finetune.params rope.finetune.params vanilla.nopretrain.dev.predictions vanilla.nopretrain.test.predictions vanilla.pretrain.dev.predictions vanilla.pretrain.test.predictions rope.pretrain.dev.predictions rope.pretrain.test.predictions london_baseline_accuracy.txt
+rm -rf ./__pycache__
+
+zip -r homework5.zip \
+  vanilla.model.params \
+  vanilla.finetune.params \
+  rope.finetune.params \
+  vanilla.nopretrain.dev.predictions \
+  vanilla.nopretrain.test.predictions \
+  vanilla.pretrain.dev.predictions \
+  vanilla.pretrain.test.predictions \
+  rope.pretrain.dev.predictions \
+  rope.pretrain.test.predictions \
+  london_baseline_accuracy.txt \
+  HWK5-simonemayers.pdf
